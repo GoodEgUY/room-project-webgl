24ac4885ea3da6f28e8d2db44989634950d98e0b
GoodEgUY <aezakmi2508@gmamil.com>
2024-01-29 18:11:00 +0200

    ddf

--- src/script.js
+++ src/script.js
@@ -1,20 +1,31 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import "./style.css"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Импорт OrbitControls
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"; // Импорт OrbitControls
 
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector(".canvas");
 const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper(10); // Длина осей
 
-
+// Добавляем оси в сцену
+scene.add(axesHelper);
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(105, window.innerWidth / window.innerHeight);
+camera.position.set(5, 5, 7);
 
+// Направление камеры
+
+
+// Угол обзора (FOV)
+camera.fov = 35; // Установка угла обзора
+camera.aspect = window.innerWidth / window.innerHeight; // Соотношение сторон
+camera.near = 0.1; // Ближняя плоскость отсечения
+camera.far = 1000; // Дальняя плоскость отсечения
+camera.updateProjectionMatrix(); // Обновление матрицы проекции
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 
@@ -35,40 +46,42 @@ controls.dampingFactor = 0.25;
 
 const loader = new GLTFLoader();
 loader.load(
-    'models/Room2/room.glb', // путь к вашей модели glTF
-    function (gltf) {
-        scene.add(gltf.scene);
-    },
-    undefined,
-    function (error) {
-        console.error(error);
-    }
+  "models/Room2/scene.glb", // путь к вашей модели glTF
+  function (gltf) {
+    gltf.scene.traverse(function(child) {
+        console.log(child.name); // Вывод имени дочерних объектов
+    });
+    
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
 );
 
 const tick = () => {
-    
-    const delta = clock.getDelta();
-    
-    // Добавьте свою логику анимации здесь, если необходимо
-    
-    controls.update(); // Обновляем управление камерой
-    
-    renderer.render(scene, camera);
+  const delta = clock.getDelta();
 
-    
-    window.requestAnimationFrame(tick);
+  // Добавьте свою логику анимации здесь, если необходимо
+
+  controls.update(); // Обновляем управление камерой
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
 };
 
 tick();
 
-window.addEventListener('resize', () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    renderer.render(scene, camera);
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
 });
